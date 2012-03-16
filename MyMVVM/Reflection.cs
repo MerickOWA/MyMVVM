fc44f2db5bbc1b49b3f957f1a6fb9a670137cbd4
Lowell Smith <merickowa@gmail.com>
2012-03-15 23:24:59 -0400

    Just a bit of work

---
+++ MyMVVM/Reflection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq.Expressions;
+
+namespace MyMVVM
+{
+  public static class Reflection
+  {
+    public static MemberInfo GetProperty( LambdaExpression propertyExpression )
+    {
+      var memberExpression = propertyExpression.Body as MemberExpression;
+
+      if ( memberExpression == null || !(memberExpression.Expression is ParameterExpression) )
+        throw new ArgumentException( "Property expression must be of the form 'x => x.Property'" );
+
+      return memberExpression.Member;
+    }
+
+    public static MemberInfo[] GetProperties( LambdaExpression propertyExpression )
+    {
+      var properties = new List<MemberInfo>();
+
+      var expression = propertyExpression.Body;
+      while ( expression is MemberExpression )
+      {
+        var memberExpression = (MemberExpression)expression;
+
+        properties.Insert( 0, memberExpression.Member );
+
+        expression = memberExpression.Expression;
+      }
+
+      if ( !(expression is ParameterExpression) )
+        throw new ArgumentException( "Property expression must be of the form 'x => x.Property1.Property2'" );
+
+      return properties.ToArray();
+    }
+  }
+}
---
+++ MyMVVM/ViewModelBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+
+namespace MyMVVM
+{
+  public class ViewModelBase : INotifyPropertyChanged, INotifyPropertyChanging
+  {
+    public void OnPropertyChanged<PropertyT>( Expression<Func<ViewModelBase, PropertyT>> property )
+    {
+      var handler = this.PropertyChanging;
+      if ( handler != null )
+        handler( this, new PropertyChangingEventArgs( Reflection.GetProperty( property ).Name ) );
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanging<PropertyT>( Expression<Func<ViewModelBase, PropertyT>> property )
+    {
+      var handler = this.PropertyChanging;
+      if ( handler != null )
+        handler( this, new PropertyChangingEventArgs( Reflection.GetProperty( property ).Name ) );
+    }
+
+    public event PropertyChangingEventHandler PropertyChanging;
+  }
+
+  public static class SetValueExtension
+  {
+    public static void SetValue<ViewModelT,PropertyT>( this ViewModelT viewModel, Expression<Func<ViewModelT,PropertyT>> property, PropertyT value, ref PropertyT field ) where ViewModelT : ViewModelBase
+    {
+      if ( !EqualityComparer<PropertyT>.Default.Equals( field, value ) )
+      {
+        viewModel.OnPropertyChanging( property );
+        field = value;
+        viewModel.OnPropertyChanged( property );
+      }
+    }
+  }
+}
